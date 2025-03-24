8f470a2d997c227e856fa5b7afd40cd693b802ed
jecktor <vek@unsteady.life>
2025-03-24 17:41:45 -0600

    register redirect

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Admin() {
+  const router = useRouter();
   const { data: session } = useSession();
   const user = session?.user as { role: number };
 
-  if (!user || user.role !== 2)
+  if (!user) {
+    return router.push("/register");
+  }
+
+  if (user.role !== 2)
     return <div>You do not have permission to access this page.</div>;
 
   return <div>Bienvenido morro</div>;
